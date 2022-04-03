3d53dc4212de534f99f4175e932b98fe1d9b3d91
fengzihao1013 <958608363@qq.com>
2022-04-03 15:38:39 +0800

    项目的创建，获取微信账号信息和openId的实现

---
+++ recruit/utils/config.js
@@ -0,0 +1,8 @@
+//配置服务器相关信息
+
+export default{
+    host: 'http://127.0.0.1:4523/mock/779286',
+    mobileHost: 'http://z438h82172.wicp.vip',
+    appId: 'wxcf656011d7b5d042',
+    secret: 'f5420dbf92fbfc046cae7a3adb78de95'
+}
